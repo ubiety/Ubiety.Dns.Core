c543876f55ec3b7c13b9d96b09042db2b75fcc32
Dieter Lunn <coder2000@gmail.com>
2018-05-20 20:21:57 -0500

    Fully cover A record

--- test/Ubiety.Dns.Test/Records/RecordATest.cs
+++ test/Ubiety.Dns.Test/Records/RecordATest.cs
@@ -8,20 +8,33 @@ namespace Ubiety.Dns.Test.Records
 {
     public class RecordATest
     {
-        [Fact]
-        public void TestAddress()
+        private readonly Byte[] data = new Byte[4];
+        private readonly RecordReader reader;
+
+        public RecordATest()
         {
-            Byte[] data = new Byte[4];
             data[0] = 192;
             data[1] = 168;
             data[2] = 0;
             data[3] = 1;
 
-            var reader = new RecordReader(data);
+            reader = new RecordReader(data);
+        }
 
+        [Fact]
+        public void TestRecordAddress()
+        {
             var a = new RecordA(reader);
 
-            Assert.Equal(a.Address, IPAddress.Parse("192.168.0.1"));
+            Assert.Equal(IPAddress.Parse("192.168.0.1"), a.Address);
+        }
+
+        [Fact]
+        public void TestRecordToString()
+        {
+            var a = new RecordA(reader);
+
+            Assert.Equal("192.168.0.1", a.ToString());
         }
     }
 }
