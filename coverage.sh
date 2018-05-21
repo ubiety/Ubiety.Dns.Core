f38599238126c651b5c9f1ec912f7d0d55ddcc8c
Dieter Lunn <coder2000@gmail.com>
2018-05-20 20:01:46 -0500

    Update coverage script

--- coverage.sh
+++ coverage.sh
@@ -4,14 +4,14 @@ dotnet build
 cd tools
 
 # Instrument assemblies inside 'test' folder to detect hits for source files inside 'src' folder
-dotnet minicover instrument --workdir ../ --assemblies src/Ubiety.Dns.Test/**/bin/**/*.dll --sources src/Ubiety.Dns.Core/**/*.cs
+dotnet minicover instrument --workdir ../ --assemblies test/**/bin/**/*.dll --sources src/**/*.cs
 
 # Reset hits count in case minicover was run for this project
 dotnet minicover reset
 
 cd ..
 
-for project in src/Ubiety.Dns.Test/**/*.csproj; do dotnet test --no-build $project; done
+for project in test/**/*.csproj; do dotnet test --no-build $project; done
 
 cd tools
 
@@ -19,10 +19,12 @@ cd tools
 dotnet minicover uninstrument --workdir ../
 
 # Create html reports inside folder coverage-html
-dotnet minicover htmlreport --workdir ../ --threshold 90
+dotnet minicover htmlreport --workdir ../ --threshold 80
 
 # Print console report
 # This command returns failure if the coverage is lower than the threshold
-dotnet minicover report --workdir ../ --threshold 90
+dotnet minicover report --workdir ../ --threshold 80
+
+dotnet minicover xmlreport --workdir ../ --threshold 80
 
 cd ..
