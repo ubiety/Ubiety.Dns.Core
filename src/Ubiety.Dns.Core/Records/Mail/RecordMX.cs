8caab46b48ed546125fcfe25f6cd0a712326b61f
Dieter Lunn <coder2000@gmail.com>
2018-06-18 16:39:08 -0500

    Move comments to documentation

--- src/Ubiety.Dns.Core/Records/Mail/RecordMX.cs
+++ src/Ubiety.Dns.Core/Records/Mail/RecordMX.cs
@@ -3,24 +3,6 @@ using System.Globalization;
 
 namespace Ubiety.Dns.Core.Records.Mail
 {
-    /*
-    3.3.9. MX RDATA format
-
-
-    where:
-
-    PREFERENCE      A 16 bit integer which specifies the preference given to
-                    this RR among others at the same owner.  Lower values
-                    are preferred.
-
-    EXCHANGE        A <domain-name> which specifies a host willing to act as
-                    a mail exchange for the owner name.
-
-    MX records cause type A additional section processing for the host
-    specified by EXCHANGE.  The use of MX RRs is explained in detail in
-    [RFC-974].
-    */
-
     /// <summary>
     /// Mail exchange DNS record
     /// </summary>
@@ -36,6 +18,19 @@ namespace Ubiety.Dns.Core.Records.Mail
     /// /                   EXCHANGE                    /
     /// /                                               /
     /// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    ///
+    /// where:
+    ///
+    /// PREFERENCE      A 16 bit integer which specifies the preference given to
+    ///                 this RR among others at the same owner.  Lower values
+    ///                 are preferred.
+    ///
+    /// EXCHANGE        A [domain-name] which specifies a host willing to act as
+    ///                 a mail exchange for the owner name.
+    ///
+    /// MX records cause type A additional section processing for the host
+    /// specified by EXCHANGE.  The use of MX RRs is explained in detail in
+    /// [RFC-974].
     /// ```
     /// </remarks>
     public sealed class RecordMx : Record, IComparable, IEquatable<RecordMx>
