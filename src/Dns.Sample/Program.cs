248d352eda6f3db728cb0428f1ef519f4d5067e2
Dieter Lunn <coder2000@gmail.com>
2018-06-21 19:25:19 -0500

    Add sample console project

---
+++ src/Dns.Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dns.Sample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var test = new DnsTest();
+            
+            Console.WriteLine("CERT records for: direct.sitenv.org");
+            foreach (var record in test.CertRecords("direct.sitenv.org"))
+            {
+                Console.WriteLine(record);
+            }
+        }
+    }
+}
